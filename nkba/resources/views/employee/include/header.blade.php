56696a3e789233405b9e8173471a32ee9277c704
aya <eng.aya136@gmail.com>
2016-06-12 20:49:56 +0200

    add exit to employee-transfer

--- nkba/resources/views/employee/include/header.blade.php
+++ nkba/resources/views/employee/include/header.blade.php
@@ -8,34 +8,30 @@
     <!--logo end-->
     <div class="top-nav notification-row">                
         <!-- notificatoin dropdown start-->
-
-        <ul class="nav pull-right top-menu">
-<li style="margin-top: 3px;">{{ link_to_route('employee-transfer.confirmed','عرض التحويلات المؤكدة') }}
-</li>
-            <!-- task notificatoin start -->
-            <li id="task_notificatoin_bar" class="dropdown">
-                <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                    <span class="icon-task-l" style="margin-right: 18px;"></span>
-                    <span class="badge bg-important" id="transfers-number"></span>
-                </a>
-                <ul class="dropdown-menu extended tasks-bar selected">
-                    <div class="notify-arrow notify-arrow-blue"></div>
-                    <li>
-                        <p class="blue">التحويلات الحالية</p>
-                    </li>
-                    <li class="external">
-                        {{ link_to_route('employee-transfer.index','عرض جميع التحويلات') }}
-                    </li>
-                </ul>
-            </li>
-            <!-- task notificatoin end -->
-
-        </ul>
-        <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>خروج</a></li>
-                            </ul>
-        <!-- notificatoin dropdown end-->
-    </div>
+       <ul class="nav pull-right top-menu">
+       <li> <a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>خروج</a></li>
+        <li style="margin-top: 3px;">{{ link_to_route('employee-transfer.confirmed','عرض التحويلات المؤكدة') }}
+        </li>
+        <!-- task notificatoin start -->
+        <li id="task_notificatoin_bar" class="dropdown">
+            <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                <span class="icon-task-l" style="margin-right: 18px;"></span>
+                <span class="badge bg-important" id="transfers-number"></span>
+            </a>
+            <ul class="dropdown-menu extended tasks-bar selected">
+                <div class="notify-arrow notify-arrow-blue"></div>
+                <li>
+                    <p class="blue">التحويلات الحالية</p>
+                </li>
+                <li class="external">
+                    {{ link_to_route('employee-transfer.index','عرض جميع التحويلات') }}
+                </li>
+            </ul>
+        </li>
+        <!-- task notificatoin end -->
+    </ul>
+    <!-- notificatoin dropdown end-->
+</div>
 </header>      
 <!--header end-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
@@ -60,7 +56,7 @@
                         for (var i = 0; i < 5; i++) {
 
                             $('.external').before('<li id="'+i+'"><a  class="tranmenu" href="{{url("employee-transfer")}}/'+data[i].id+'"><div class="task-info">'+
-                             '<div class="desc">تحويلة رقم '+data[i].id+'</div></div></a></li>');
+                               '<div class="desc">تحويلة رقم '+data[i].id+'</div></div></a></li>');
                         }
                     }
                     setTimeout(receiveTransfers,1000);
